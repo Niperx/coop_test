d880adbabcb0ba749275be54b34189dd1706c7e9
Kharon <kharon98@gmail.com>
2023-03-22 17:57:48 +1100

    Bot 3.5

--- test.py
+++ test.py
@@ -1,43 +1,47 @@
 import openai
-openai.api_key = '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'
-aaa = input()
-messages = []
-print('asd')
-who_is = input("Кем будет являться чат GPT в данном диалоге: ")
-while True:
-    content = input("User: ")
-    messages.append({"role": "assistant", "content": who_is})
+import aiogram
+import config
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+openai.api_key = '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'
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class ChatStage(StatesGroup):
+    waiting_for_request = State()
+
+
+def get_answer(input_text, messages):
+    content = input_text
     messages.append({"role": "user", "content": content})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     chat_response = completion.choices[0].message.content
-    print(f'ChatGPT: {chat_response}')
     messages.append({"role": "assistant", "content": chat_response})
-
-# response = openai.Image.create(
-#     prompt=aaa,
-#     n=1,
-#     size="256x256",
-# )
-#
-# print(response["data"][0]["url"])
+    return messages
 
 
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message, state: FSMContext):
+    messages = []
+    await state.update_data(info=messages)
+    await message.answer("Разговорный бот, пиши что хочешь")
 
-# @dp.message_handler(commands=['start'])
-# async def start(message: types.Message):
-#     await message.answer("Привет!\n"
-#                          "Отвечу на вопросы мира")
-#
-#
-# @dp.message_handler(content_types=['text'])
-# async def answer(message: types.Message):
-#     question = message.text
-#     ans = get_answer(question)
-#     await message.answer(ans)
-#
-#
-# if __name__ == '__main__':
-#     aiogram.executor.start_polling(dp)
+
+@dp.message_handler(content_types=['text'])
+async def answer(message: types.Message, state: FSMContext):
+    info = await state.get_data()
+    ans = get_answer(message.text, info['info'])
+    print(ans)
+    await state.update_data(info=ans)
+    await message.answer(ans[-1]['content'])
+
+
+if __name__ == '__main__':
+    aiogram.executor.start_polling(dp)
